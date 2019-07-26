e5ae37120ef9293e24aede6ab7d5ef8a638f9679
Ben Krikler <bek07@ic.ac.uk>
2019-07-26 19:55:20 +0200

    Add compile_sequence_yaml to __all__

--- fast_flow/v1/__init__.py
+++ fast_flow/v1/__init__.py
@@ -3,7 +3,8 @@ from .dict_config import read_sequence_dict, compile_sequence_dict
 from .yaml_config import config_dict_from_yaml
 
 
-__all__ = ["read_sequence_yaml", "read_sequence_dict", "compile_sequence_dict"]
+__all__ = ["read_sequence_yaml", "compile_sequence_yaml",
+           "read_sequence_dict", "compile_sequence_dict"]
 
 
 def read_sequence_yaml(cfg_filename, output_dir=None, backend=None):
