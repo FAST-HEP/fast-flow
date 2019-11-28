0765a57dc46344828842afd0244db7fed76d2a80
Ben Krikler <bek07@ic.ac.uk>
2019-11-28 20:03:38 +0100

    Rearrange versioning

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+import os
 import setuptools
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def get_version():
+    _globals = {}
+    with open(os.path.join("fast_flow", "version.py")) as version_file:
+        exec(version_file.read(), _globals)
+    return _globals["__version__"]
+
+
 setuptools.setup(
     name="fast-flow",
-    version="0.3.0",
+    version=get_version(),
     author="Ben Krikler",
     author_email="fast-hep@cern.ch",
     description="YAML-based analysis flow description language",
