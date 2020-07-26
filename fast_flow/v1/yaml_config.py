d11d9607c83e0d8f29b44a11613d823d6be2c371
Ben Krikler <bek07@ic.ac.uk>
2020-07-26 18:07:39 +0200

    Refactor import mechanism for book-keeping purposes

--- fast_flow/v1/yaml_config.py
+++ fast_flow/v1/yaml_config.py
@@ -1,4 +1,6 @@
 import os
+import copy
+from .dict_config import infer_stage_name_class
 
 
 def config_dict_from_yaml(cfg_filename, output_dir=None, backend=None):
@@ -15,4 +17,28 @@ def config_dict_from_yaml(cfg_filename, output_dir=None, backend=None):
         cfg["general"]["backend"] = backend
     cfg["general"]["this_dir"] = os.path.dirname(cfg_filename)
 
+    cfg = expand_imports(cfg, output_dir=output_dir, backend=backend)
+
     return cfg
+
+
+def expand_imports(cfg, output_dir=None, backend=None):
+    expanded_stages = []
+    expanded_cfg = copy.deepcopy(cfg)
+    stages = expanded_cfg.pop("stages")
+    this_dir = expanded_cfg["general"]["this_dir"]
+
+    for i, stage_cfg in enumerate(stages):
+        name, target = infer_stage_name_class(i, stage_cfg)
+        if name == "IMPORT":
+            target = target.format(this_dir=this_dir)
+            expanded = config_dict_from_yaml(target, output_dir=output_dir, backend=backend)
+            expanded_stages += expanded.pop("stages")
+            # TODO: Add check that imported satages dont overwrite existing ones
+            expanded_cfg.update(expanded)
+            continue
+        if name in expanded_stages:
+            raise ValueError("Repeated stage name: '%s'" % name)
+        expanded_stages.append(stage_cfg)
+    expanded_cfg["stages"] = expanded_stages
+    return expanded_cfg
